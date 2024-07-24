8662f0c98108c0b8b1752a6cf219beb40239e252
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-07-24 14:14:43 -0300

    Adicionando cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 app.use(express.json());
 require('dotenv').config();
 const port = process.env.API_PORT;
@@ -7,8 +8,13 @@ const port = process.env.API_PORT;
 const usuarioRouter = require('./routers/UsuarioRouter');
 const ocorrenciaRouter = require('./routers/OcorrenciaRouter');
 
+let corsOptions = {
+  origin : ['http://localhost:5500'],
+}
+
+app.use(cors());
 app.use('/usuarios', usuarioRouter);
-app.use('/ocorrencias', ocorrenciaRouter);
+app.use('/ocorrencias', cors(corsOptions),  ocorrenciaRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
